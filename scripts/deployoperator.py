96ecdbb9a3853020248cc6f9b1e625b1094a200c
Jason-Kahn <jason.kahn@ibm.com>
2025-04-30 15:01:06 -0400

    Release of Filenet Content Manager 5.6.0-IF003

--- scripts/deployoperator.py
+++ scripts/deployoperator.py
@@ -29,7 +29,7 @@ from helper_scripts.utilities.interface import clear, display_issues, display_pr
 from helper_scripts.utilities.utilities import prereq_checks, read_version_toml, create_deployment_info, \
     create_version_info
 
-__version__ = "4.4.2"
+__version__ = "4.4.4"
 
 app = typer.Typer()
 state = {
@@ -75,28 +75,25 @@ def display_mode_version(mode: str, description: str):
     print()
 
 
-def setup_logger(file_log_level, verbose=False):
+def setup_logger(file_log_level):
     # Create a logger object
-    logger = logging.getLogger("deployoperator")
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
+    # Setup console logger
     shell_handler = RichHandler()
-    file_handler = logging.FileHandler("deployoperator.log")
-
-    logger.setLevel(file_log_level)
     shell_handler.setLevel(file_log_level)
+    formatter_rich = logging.Formatter("%(message)s")
+    shell_handler.setFormatter(formatter_rich)
+
+    # Setup file logger
+    file_handler = logging.FileHandler("deployoperator.log")
     file_handler.setLevel(logging.DEBUG)
+    formatter_file = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)-100s - %(filename)s:%(lineno)d", "%Y-%m-%d %H:%M:%S")
+    file_handler.setFormatter(formatter_file)
 
-    # the formatter determines what our logs will look like
-    fmt_shell = '%(message)s'
-    fmt_file = '%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
-
-    shell_formatter = logging.Formatter(fmt_shell)
-    file_formatter = logging.Formatter(fmt_file)
-
-    # here we hook everything together
-    shell_handler.setFormatter(shell_formatter)
-    file_handler.setFormatter(file_formatter)
-
+    # Add handlers to the logger
     logger.addHandler(shell_handler)
     logger.addHandler(file_handler)
 
