2764c406760d5f6397b6a3982747a57baa71955e
Haresh-Gautham-S <Haresh.Gautham.S1@ibm.com>
2025-08-28 16:37:41 +0000

    Release of FNCM DevOps Suite 5.1.2
    Release of FNCM DevOps Suite 5.1.2

--- scripts/deployoperator.py
+++ scripts/deployoperator.py
@@ -29,7 +29,7 @@ from helper_scripts.utilities.interface import clear, display_issues, display_pr
 from helper_scripts.utilities.utilities import prereq_checks, read_version_toml, create_deployment_info, \
     create_version_info
 
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 
 app = typer.Typer()
 state = {
