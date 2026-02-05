b86f744550b6f454b52228969057245dbd521c09
Jason Kahn <jason.kahn@ibm.com>
2026-02-05 14:48:44 -0500

    Release of FNCM DevOps Suite 7.1.6

--- scripts/loadimages.py
+++ scripts/loadimages.py
@@ -39,7 +39,7 @@ from helper_scripts.utilities.interface import clear, display_issues, display_pr
 from helper_scripts.utilities.utilities import validate_image_details_file, prereq_checks, read_version_toml, \
     validate_airgap_details_file
 
-__version__ = "7.1.5"
+__version__ = "7.1.6"
 
 app = typer.Typer()
 
